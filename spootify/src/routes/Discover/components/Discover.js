1a59efd9ada2765d4ac39e2fd244722a37b0aabb
Juli Cheng <julich3ng@gmail.com>
2023-02-01 17:08:13 +0000

    feat: load data from apis in parallel

--- spootify/src/routes/Discover/components/Discover.js
+++ spootify/src/routes/Discover/components/Discover.js
@@ -1,27 +1,48 @@
-import React, { Component } from 'react';
-import DiscoverBlock from './DiscoverBlock/components/DiscoverBlock';
-import '../styles/_discover.scss';
+import React, { useEffect, useState } from "react";
+import DiscoverBlock from "./DiscoverBlock/components/DiscoverBlock";
+import "../styles/_discover.scss";
+import api from '../../../api'
 
-export default class Discover extends Component {
-  constructor() {
-    super();
+export default function Discover() {
+  const [newReleases, setNewReleases] = useState([]);
+  const [playlists, setPlaylists] = useState([]);
+  const [categories, setCategories] = useState([]);
 
-    this.state = {
-      newReleases: [],
-      playlists: [],
-      categories: []
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Call multiple APIs in parallel
+        const [releasesRes, playlistsRes, categoriesRes] = await Promise.all([
+          api.albums.getNewReleases(),
+          api.playlists.getFeaturedPlaylists(),
+          api.categories.getCategories()
+        ]);
+  
+        setNewReleases(releasesRes || [])
+        setPlaylists(playlistsRes || [])
+        setCategories(categoriesRes || [])
+      } catch (e) {
+        console.error(e)
+      }
     };
-  }
 
-  render() {
-    const { newReleases, playlists, categories } = this.state;
+    fetchData();
+  }, []);
 
-    return (
-      <div className="discover">
-        <DiscoverBlock text="RELEASED THIS WEEK" id="released" data={newReleases} />
-        <DiscoverBlock text="FEATURED PLAYLISTS" id="featured" data={playlists} />
-        <DiscoverBlock text="BROWSE" id="browse" data={categories} imagesKey="icons" />
-      </div>
-    );
-  }
+  return (
+    <div className="discover">
+      <DiscoverBlock
+        text="RELEASED THIS WEEK"
+        id="released"
+        data={newReleases}
+      />
+      <DiscoverBlock text="FEATURED PLAYLISTS" id="featured" data={playlists} />
+      <DiscoverBlock
+        text="BROWSE"
+        id="browse"
+        data={categories}
+        imagesKey="icons"
+      />
+    </div>
+  );
 }
